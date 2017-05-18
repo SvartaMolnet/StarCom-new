ed4b5c774b722b02fc8e9d72a5fb2ad895d4d776
SvartaMolnet <svartamolnet@live.com>
2017-05-18 04:45:37 +0200

    NewStarCom

---
+++ changeBackground.js
@@ -0,0 +1,7 @@
+function changeimage() {
+    var a = "relic";
+    var c = a + ".jpg";
+
+    //document.getElementById('image').src = c;
+    document.getElementById("new").style.backgroundImage = "url('mining-bg.png')";
+}
